982d0ffba1bc918e72d96ae972600ebcc3389d0d
Nicolas Pouillard <np@users.noreply.github.com>
2025-08-28 23:53:25 +0200

    feat: add docker image target

--- flake.nix
+++ flake.nix
@@ -13,7 +13,21 @@
         haskellPackages = pkgs.haskellPackages;
         notion-to-memgraph = haskellPackages.callCabal2nix "notion-to-memgraph" ./. {};
       in {
-        packages.default = notion-to-memgraph;
+        packages = {
+          default = notion-to-memgraph;
+          dockerImage = pkgs.dockerTools.buildImage {
+            name = "notion-to-memgraph";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [ notion-to-memgraph ];
+              pathsToLink = [ "/bin" ];
+            };
+            config = {
+              Cmd = [ "/bin/notion-to-memgraph-web" ];
+              ExposedPorts = { "8080/tcp" = {}; };
+            };
+          };
+        };
         devShells.default = pkgs.mkShell {
           inputsFrom = [ notion-to-memgraph ];
         };
